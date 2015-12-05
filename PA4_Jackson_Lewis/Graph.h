116c63b007de75b6d0b28744bd949b528d38aa74
Justin Jackson <jackson.jt@att.net>
2015-12-04 21:45:25 -0800

    sdfg

--- PA4_Jackson_Lewis/Graph.h
+++ PA4_Jackson_Lewis/Graph.h
@@ -9,26 +9,30 @@
 // Class definition
 class graph
 {
+
+
 public:
 	// Public vars
-	std::unordered_map<int, vertex> _vertices;
+	unordered_map<int, vertex> _vertices;
 
 	// Constructors
 	graph();
-	graph(std::unordered_map<int, vertex> new_vertices);
+	graph(unordered_map<int, vertex> new_vertices);
 
 	// Copy Constructor
 
 	// Destructor
 
 	// Getters --- ADDITIONAL GETTERS ARE NEEDED FOR MANIPULATION OF INDIVIDUAL VERTICES probably - get_vertex_at(key), returning value
-	const std::unordered_map<int, vertex> get_vertices();
+	unordered_map<int, vertex> get_vertices() const;
 
 	// Setters --- ADDITIONAL SETTERS ARE NEEDED FOR MANIPULATION OF INDIVIDUAL VERTICES probably - set_vertex_at(key, value)
-	void set_vertices(std::unordered_map<int, vertex> new_vertices);
+	void set_vertices(unordered_map<int, vertex> new_vertices);
 
 	// Methods
-	path computeShortestPath(vertex* start);
+	unordered_map<vertex, int> computeShortestPath(vertex* start);
+
+
 
 private:
 };
