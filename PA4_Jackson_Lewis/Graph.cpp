88d23a5670748f371393f4b8698293075b2a265c
Andrew Lewis <andrewjohnewis@hotmail.com>
2015-12-04 19:22:14 -0800

    Adams graph from the MA 12

--- PA4_Jackson_Lewis/Graph.cpp
+++ PA4_Jackson_Lewis/Graph.cpp
@@ -42,5 +42,55 @@ path graph::computeShortestPath(vertex* start)
 		(weight = vertex.getWeight() * vertext.getLoadFactor()).
 	*/
 
+	//holds known distances
+	unordered_map<vertex, int> distances;
+
+	//underlying heap
+	priority_queue<vertex, vector<vertex>, PathWeightComparer> dijkstra_queue{};
+
+	//reset start's path weight
+	start->setPathWeight(0);
+
+	//make sure that the starting vertex is in the graph
+	if (_vertices.find(start->get_id()) != _vertices.end())
+	{
+		//push on starting vertex
+		dijkstra_queue.push(*start);
+
+		//while queue not empty
+		while (dijkstra_queue.empty() == false)
+		{
+			//push on outgoing edges that haven't been discovered
+			vertex top = dijkstra_queue.top();
+			dijkstra_queue.pop();
+
+			//Top of heap not known (in distances)?
+			if (distances.find(top) == distances.end())
+			{
+				//make known
+				int current_path_weight = top.getPathWeight();
+				distances[top] = current_path_weight;
+
+				//push on outgoing edges
+				for (auto item : top.get_edges())
+				{
+					vertex *next = item.first;
+					int weight = item.second;
+					next->setPathWeight(weight + current_path_weight);
+
+					//not known?  add to heap
+					if (distances.find(*next) == distances.end())
+					{
+						dijkstra_queue.push(*next);
+					}
+				}
+			}
+		}
+
+		return distances;
+	}
+
+
+
 	return path();
 }
--- PA4_Jackson_Lewis/Vertex.h
+++ PA4_Jackson_Lewis/Vertex.h
@@ -10,7 +10,7 @@ class vertex
 public:
 	// Public vars
 	int _id = 0;
-	std::unordered_map<vertex*, int> _edges;
+	unordered_map<vertex*, int> _edges;
 	int _load_factor = 0;
 
 	// Constructors
@@ -22,18 +22,71 @@ public:
 	// Destructor
 
 	// Getters
-	const int vertex::get_id();
-	const std::unordered_map<vertex*, int> vertex::get_edges();
-	const int vertex::get_load_factor();
+	int vertex::get_id() const;
+	unordered_map<vertex*, int> vertex::get_edges();
+	int vertex::get_load_factor() const;
+	int getPathWeight() const;
 
 	// Setters
 	void set_id(int new_id);
 	void set_edges(std::unordered_map<vertex*, int> new_edges);
 	void set_load_factor(int new_load_factor);
+	void setPathWeight(int weight);
 
 	// Methods
-
 private:
+	int _path_weight = 0;
+	static int _id_counter;
 };
+
+int operator==(const vertex &lhs, const vertex &rhs)
+{
+	return lhs.get_id() == rhs.get_id();
+}
+
+bool operator<(const vertex &lhs, const vertex &rhs)
+{
+	return lhs.get_id() < rhs.get_id();
+}
+
+bool operator>(const vertex &lhs, const vertex &rhs)
+{
+	return lhs.get_id() > rhs.get_id();
+}
+
+class PathWeightComparer
+{
+public:
+	bool operator()(const vertex lhs, const vertex rhs)
+	{
+		return (lhs.getPathWeight() > rhs.getPathWeight());
+	}
+};
+
+//hashing algorithm must exist in STD namespace
+namespace std {
+
+	template <>
+	struct hash<vertex>
+	{
+		//provide a hash (convert grocery item into integer)
+		std::size_t operator()(const vertex& item) const
+		{
+			size_t hash_val = 0;
+
+			//to hash INTs using the STL
+			hash<int> int_hash{};
+
+			//define hashing algorithm.  Ours is pretty easy...
+			hash_val = int_hash(item.get_id());
+
+			//add others as necessary
+			return hash_val;
+		}
+	};
+}
+
+
+
 
 #endif
