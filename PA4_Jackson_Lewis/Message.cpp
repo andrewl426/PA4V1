849975915ffb106dd8926b499ab0698a5f0ed76d
Justin Jackson <jackson.jt@att.net>
2015-12-07 00:16:04 -0800

    Forgot files

--- PA4_Jackson_Lewis/Message.cpp
+++ PA4_Jackson_Lewis/Message.cpp
@@ -8,7 +8,7 @@ message::message()
 	//_starting_vertex = empty!
 	//_ending_vertex = empty!
 }
-message::message(queue<packet> new_packets, vertex* new_starting_vertex, vertex* new_ending_vertex)
+message::message(queue<packet> new_packets, vertex new_starting_vertex, vertex new_ending_vertex)
 {
 	_packets = new_packets;
 	_starting_vertex = new_starting_vertex;
@@ -24,11 +24,11 @@ const queue<packet> message::get_packets()
 {
 	return _packets;
 }
-const vertex* message::get_starting_vertex()
+const vertex message::get_starting_vertex()
 {
 	return _starting_vertex;
 }
-const vertex* message::get_ending_vertex()
+const vertex message::get_ending_vertex()
 {
 	return _ending_vertex;
 }
@@ -38,11 +38,11 @@ void message::set_packets(queue<packet> new_packets)
 {
 	_packets = new_packets;
 }
-void message::set_starting_vertex(vertex * new_starting_vertex)
+void message::set_starting_vertex(vertex new_starting_vertex)
 {
 	_starting_vertex = new_starting_vertex;
 }
-void message::set_ending_vertex(vertex * new_ending_vertex)
+void message::set_ending_vertex(vertex new_ending_vertex)
 {
 	_ending_vertex = new_ending_vertex;
 }
