ec07a8d88793e14974a8f631451f13e9bfc5f50e
Andrew Lewis <andrewjohnewis@hotmail.com>
2015-12-07 23:46:57 -0800

    prevent crash

--- PA4_Jackson_Lewis/Graph.cpp
+++ PA4_Jackson_Lewis/Graph.cpp
@@ -57,6 +57,7 @@ unordered_map<vertex, int> graph::computeShortestPath(vertex start)
 	
 	//holds known distances
 	unordered_map<vertex, int> distances;
+
 	path for_the_stack;
 	//underlying heap
 	priority_queue<vertex, vector<vertex>, PathWeightComparer> dijkstra_queue{};
@@ -72,6 +73,7 @@ cout << endl << "start.path_weight: " << start.getPathWeight();
 	{
 		//push on starting vertex
 		dijkstra_queue.push(start);
+		//for_the_stack.push_vertex(start); //testing
 cout << endl << "Push on start(id): " << start.get_id();
 
 		//while queue not empty
@@ -80,7 +82,7 @@ cout << endl << "Push on start(id): " << start.get_id();
 			//push on outgoing edges that haven't been discovered
 			vertex top = dijkstra_queue.top();
 			dijkstra_queue.pop();
-			for_the_stack.pop_vertex();
+			//for_the_stack.pop_vertex(); //testing
 
 			//Top of heap not known (in distances)?
 			if (distances.find(top) == distances.end())
@@ -92,7 +94,6 @@ cout << endl << "Push on start(id): " << start.get_id();
 				//cout << "Current path weight: " << current_path_weight << endl;
 				distances[top] = current_path_weight;
 				
-
 cout << endl << endl << "vertex top(id): " << top.get_id();
 cout << endl << "vertex top(PW): " << top.getPathWeight();
 cout << endl << "vertex top(LF): " << top.get_load_factor();
@@ -109,7 +110,7 @@ cout << endl << "vertex top(LF): " << top.get_load_factor();
 					if (distances.find(*next) == distances.end())
 					{
 						dijkstra_queue.push(*next);
-						for_the_stack.push_vertex(top);
+						//for_the_stack.push_vertex(top);
 					}
 				}
 			}
